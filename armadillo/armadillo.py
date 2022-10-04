584f13f66fed8ef449ca44b561b95c97c324b5df
TristanSJones <109077469+TristanSJones@users.noreply.github.com>
2022-10-04 17:39:24 +0100

    Trajectory implementation and bug fix (#10)
    * simplify find_pi_stacking_rings function
    
    * Doc-String alteration
    
    * Seperates t-shaped rings from stacking list
    
    * Addition of graph plotting function
    
    * Fixes ring_a mistake
    
    * Bug fixes trajectory
    
    Co-authored-by: Micaela Matta <31009265+micaela-matta@users.noreply.github.com>

--- armadillo/armadillo.py
+++ armadillo/armadillo.py
@@ -26,13 +26,15 @@ def unit_vector(ag):
     vec = ag[1].position - ag[0].position
     return vec / np.sqrt((vec * vec).sum())
 
-def find_all_rings(u):
+def find_all_rings(ag, u):
     """
     Finds all the rings within a given universe
     Each ring found is a atom group and so a list of rings is created
 
     Parameters
     -----------
+    ag :
+        AtomGroup that contains all the atoms that make up the potential rings
     u :
         MDAnalysis Universe containing topology and trajectory info
 
@@ -41,7 +43,7 @@ def find_all_rings(u):
     List of atom groups where each atom group is a ring
      """
     g = nx.Graph()
-    g.add_edges_from(u.bonds.to_indices())
+    g.add_edges_from(ag.bonds.to_indices())
     # cycle_basis gives all rings
     # but a fused ring
     cycles = nx.cycle_basis(g)
@@ -60,7 +62,7 @@ def ring_center(self):
 
 def normal_vector(ring):
     """
-    Takes a given ring, creates two planes. One plane is between atom 1 and atom 3,
+     Takes a given ring, creates two planes. One plane is between atom 1 and atom 3,
      the second plane is between atom 2 and atom 4.
      The unit vector of these planes is then calculated and from this
      the nomral is determind via the cross product of the unit vectors
@@ -79,7 +81,7 @@ def normal_vector(ring):
     normal = np.cross(v1, v2) #Calculates normal via cross product method
     return normal / np.linalg.norm(normal)
 
-def ring_distance(ring_a, ring_b, u):
+def ring_distance(ring_a, ring_b, ag):
     """
     Caluclates the distance between the centre of geometry of two Rings by using
     calc_bonds function with MDAnalysis. The box used is set as u.dimensions
@@ -90,8 +92,8 @@ def ring_distance(ring_a, ring_b, u):
         Atom group containing the atoms of one ring
     ring_b :
         Atom group containing the atoms of one ring
-    u :
-        MDAnalysis Universe containing topology and trajectory info
+    ag :
+        AtomGroup that contains all the atoms that make up the potential rings
 
     Returns
     --------
@@ -99,16 +101,13 @@ def ring_distance(ring_a, ring_b, u):
     """
     center_a = ring_center(ring_a)
     center_b = ring_center(ring_b)
-    Distance = distances.calc_bonds(center_a, center_b, box=u.dimensions)
+    Distance = distances.calc_bonds(center_a, center_b, box=ag.dimensions)
     #Takes two center geometry coords and caluclates distance between
     return Distance
 
 def ring_normal_angle(ring_a, ring_b):
     """
-    Calculates the alpha angle between the center of geometry of two rings
-    This is done by calculating the normal vector of both rings and then
-    the cosine square is calculated by dot product of the two vectors divided
-    by the normal vectors of both multiplied together, all squared
+    alpha angle = (arccos[(xa * xb + ya * yb + za * zb) / (√(xa^2 + ya^2 + za^2) * √(xb^2 + yb^2 + zb^2))]])^2
 
     Parameters
     -----------
@@ -124,7 +123,7 @@ def ring_normal_angle(ring_a, ring_b):
     vector_a = normal_vector(ring_a)
     vector_b = normal_vector(ring_b)
     #calcs the normal vector of both rings
-    cos_normal = np.dot(vector_a, vector_b/(np.linalg.norm(vector_a)*np.linalg.norm(vector_b)))
+    cos_normal = np.dot(vector_a, vector_b/np.sqrt(np.linalg.norm(vector_a)*np.linalg.norm(vector_b)))
     #calcs the cos of between the two rings
     cos_squar_normal = cos_normal*cos_normal
     #calcs the cos squar so that normalised to 90 deg
@@ -132,7 +131,7 @@ def ring_normal_angle(ring_a, ring_b):
     #calcs the alpha angle
     return alpha_angle
 
-def pi_stacking(ring_a, ring_b, u, dist_max, alpha_max):
+def pi_stacking(ring_a, ring_b, ag, dist_max, alpha_max):
     """
     Function tests wether pi_stacking occures or not by setting constaints
     on what is classed as pi-stacking and what is not. If the rings are below
@@ -144,8 +143,8 @@ def pi_stacking(ring_a, ring_b, u, dist_max, alpha_max):
         Atom group containing the atoms of one ring
     ring_b :
         Atom group containing the atoms of one ring
-    u :
-        MDAnalysis Universe containing topology and trajectory info
+    ag :
+        AtomGroup that contains all the atoms that make up the potential rings
     dist_max : float
         Maximum cutoff distance between a ring pair
     alpha_max : float
@@ -156,11 +155,11 @@ def pi_stacking(ring_a, ring_b, u, dist_max, alpha_max):
     --------
     True if the rings are pi-satcking, false if the rings are mot pi-stacking
     """
-    return ring_distance(ring_a, ring_b, u) < dist_max and ring_normal_angle(ring_a, ring_b) < alpha_max
+    return ring_distance(ring_a, ring_b, ag) < dist_max and ring_normal_angle(ring_a, ring_b) < alpha_max
     #distance between them is smaller than max distance and angle between them is smaller than max angle then pi stacking is seen
 
-def find_pi_stacking_rings(ring_list, u, distmax, distmin,
-                           angle_threshold_parallel, angle_threshold_tshaped):
+def find_pi_stacking_rings(ring_list, ag, distmax=5.0, distmin=2.0,
+                           angle_threshold_parallel=45, angle_threshold_tshaped=80):
     '''
     loops over an array-like list of ring objects. check if they are pi-stacking based on
     distance, where the max=5.0 and min=2.0
@@ -172,8 +171,8 @@ def find_pi_stacking_rings(ring_list, u, distmax, distmin,
     -----------
     ring_list :
         List of atom groups which are rings to be stacked
-    u :
-        MDAnalysis Universe containing topology and trajectory info
+    ag :
+        AtomGroup that contains all the atoms that make up the potential rings
     distmax : float
         Maximum cutoff distance between a ring pair
     distmin : float
@@ -185,7 +184,11 @@ def find_pi_stacking_rings(ring_list, u, distmax, distmin,
 
     Returns
     --------
-    Returns a lists of all the pi_stacking rings
+    ix_of_actually_pi_stacking : 
+        list of  pi-stacking rings
+    
+    ix_t_shaped_pi_stacking : 
+        list of t-shaped stacking rings
     '''
 
     ring_cogs = []
@@ -200,7 +203,7 @@ def find_pi_stacking_rings(ring_list, u, distmax, distmin,
     ix_rings = mda.lib.distances.self_capped_distance(ring_cogs_array,
                                                     max_cutoff=distmax,
                                                     min_cutoff=distmin,
-                                                    box=u.dimensions, return_distances=False)
+                                                    box=ag.dimensions, return_distances=False)
 
     # checks if ring pairs are in same fragment, if so, we don't want to calculate pi stacking!
     ring_couples_to_stack = []
@@ -212,20 +215,104 @@ def find_pi_stacking_rings(ring_list, u, distmax, distmin,
     # now check angle between normal of ring a and distance vector
     ix_of_actually_pi_stacking=[]
 
+    ix_t_shaped_pi_stacking=[]
+
     for a, b in ring_couples_to_stack:
 
         vector_a = normal_vector(ring_list[a])
         vector_b = normal_vector(ring_list[b])
         #calculate angle between 2 vectors
-        cos = (np.dot(vector_a, vector_b/(np.linalg.norm(vector_a)*np.linalg.norm(vector_b))))
+        cos = (np.dot(vector_a, vector_b/np.sqrt(np.linalg.norm(vector_a)*np.linalg.norm(vector_b))))
         #calcs cos for the two rings
         cos_squar = cos*cos
         #cosine squared so that the angle is within 0-90 deg for alpha
         alpha = np.rad2deg(np.arccos(cos_squar)) #calculates the angle
 
 
-        if alpha < angle_threshold_parallel or alpha > angle_threshold_tshaped:
+        if alpha < angle_threshold_parallel:
             #angle_theshold for stacking chosen by user
             ix_of_actually_pi_stacking.append([a, b])
 
-    return ix_of_actually_pi_stacking
+        if alpha >angle_threshold_tshaped:
+            #angle_theshold_tshaped for t_shaped stacking chosen by user
+            ix_t_shaped_pi_stacking.append([a, b])
+
+    return ix_of_actually_pi_stacking, ix_t_shaped_pi_stacking
+
+def pi_stacking_distribution(parallel_stacking, t_shaped, ring_list, ag, u, frame):
+    """
+    Function plots a graph showing the distribution of inter-ring distances and angles
+    for pi-stacking rings
+
+    Parameters
+    -----------
+    parallel_stacking :
+        List of atoms groups which are the parrallel pi-stacking ring pairs
+    t_shaped :
+        List of atoms groups which are the t-shaphed pi-stacking ring pairs
+    ring_list :
+        List of atom groups which are rings to be stacked
+    ag :
+        AtomGroup that contains all the atoms that make up the potential rings
+    u :
+        MDAnalysis Universe containing topology and trajectory info
+    frame : float
+        The frame in which the topology and trajectory data has come from
+
+
+    Returns
+    --------
+    Scatter plot of all the stacking rings. With parallel coloured in blue and
+    t-shaped coloured in red
+    """
+
+    u.trajectory[frame]
+
+    #Calculating alpha for the parrallel pi-stacking ring pairs
+    alpha_pi_stacking=[]
+    for a, b in parallel_stacking:
+        vector_a = normal_vector(ring_list[a])
+        vector_b = normal_vector(ring_list[b])
+
+        alpha_stack = ring_normal_angle(ring_list[a], ring_list[b])
+        alpha_pi_stacking.append([alpha_stack])
+
+    #Calculating distance between the parrallel pi-stacking ring pairs
+    distance_pi_stacking = []
+    for a, b in parallel_stacking:
+        center_a = ring_list[a].center_of_geometry()
+        center_b = ring_list[b].center_of_geometry()
+
+        distance = ring_distance(ring_list[a], ring_list[b], ag)
+        distance_pi_stacking.append(distance)
+
+    #Calculating alpha for the t-shaped pi-stacking ring pairs
+    alpha_t_shaped_stacking = []
+    for a, b in t_shaped:
+        vector_a = normal_vector(ring_list[a])
+        vector_b = normal_vector(ring_list[b])
+
+        alpha_t = ring_normal_angle(ring_list[a], ring_list[b])
+        alpha_t_shaped_stacking.append([alpha_t])
+
+    #Calculating distance between the t-shaped pi-stacking ring pairs
+    distance_t_shaped_stacking = []
+    for a, b in t_shaped:
+        center_a = ring_list[a].center_of_geometry()
+        center_b = ring_list[b].center_of_geometry()
+
+        distance_t = ring_distance(ring_list[a], ring_list[b], ag)
+        distance_t_shaped_stacking.append(distance_t)
+
+    #plotting scatter graph
+    fig=plt.figure()
+    ax=fig.add_axes([0,0,1,1])
+    ax.scatter(distance_pi_stacking, alpha_pi_stacking, color='b', label='Parallel', marker='x')
+    ax.scatter(distance_t_shaped_stacking, alpha_t_shaped_stacking, color='r', label='T-Shaped', marker='x')
+    ax.set_xlabel('Distance between Center of Geometry (Å)')
+    ax.set_ylabel('Alpha (deg)')
+    ax.set_title('π-Stacking Rings')
+    plt.legend(loc="upper left")
+
+    return plt.show()
+
--- armadillo/tests/test_functions.py
+++ armadillo/tests/test_functions.py
@@ -12,23 +12,26 @@ def test_armadillo_imported():
 u = mda.Universe('g3.pdb', guess_bonds=True)
 #test file with known amount of rings
 
+ag = (u.atoms.select_atoms('name C2 C3 S SG'))
+#defines a atom group of all the atoms that make up potential rings
+
 def test_find_all_rings():
     """"Tests that the find_all_rings function finds the correct number of rings
     in the example universe"""
-    assert len(armadillo.find_all_rings(u)) == 767
+    assert len(armadillo.find_all_rings(ag, u)) == 767
 
 
 def test_unit_vector():
     """Test that unit_vector functions find a error when a atom group of more
     than two atoms is passed through it"""
-    ring_list = armadillo.find_all_rings(u)
+    ring_list = armadillo.find_all_rings(ag, u)
     ag = ring_list[0]
     assert armadillo.unit_vector(ag) == 'ERROR'
 
 
 def test_normal_vector():
     """Test that the normal_vector function finds a three dimensional vector"""
-    ring_list = armadillo.find_all_rings(u)
+    ring_list = armadillo.find_all_rings(ag, u)
     ring = ring_list[0]
     assert len(armadillo.normal_vector(ring)) == 3
 
@@ -36,7 +39,7 @@ def test_normal_vector():
 def test_ring_normal_angle():
     """Tests that ring_normal_angle returns the correct angle between two rings
     that have a known angle between them"""
-    ring_list = armadillo.find_all_rings(u)
+    ring_list = armadillo.find_all_rings(ag, u)
     ring_a = ring_list[0]
     ring_b = ring_list[1]
     assert armadillo.ring_normal_angle(ring_a, ring_b) == pytest.approx(67.65, 0.05)
@@ -44,7 +47,7 @@ def test_ring_normal_angle():
 def test_ring_distance():
     """Tests that ring_distance returns the correct distance between the centers
     of two rings that have a known distance between them"""
-    ring_list = armadillo.find_all_rings(u)
+    ring_list = armadillo.find_all_rings(ag, u)
     ring_a = ring_list[0]
     ring_b = ring_list[1]
     assert armadillo.ring_distance(ring_a, ring_b, u) == pytest.approx(3.948, 0.01)
@@ -52,7 +55,7 @@ def test_ring_distance():
 def test_pi_stacking_individual():
     """Tests that when two rings that are known not to stack due to alpha angle
     being too large a false value is returned"""
-    ring_list = armadillo.find_all_rings(u)
+    ring_list = armadillo.find_all_rings(ag, u)
     ring_a = ring_list[0]
     ring_b = ring_list[1]
     assert armadillo.pi_stacking(ring_a, ring_b, u, dist_max=5.0, alpha_max=45) == False
@@ -60,7 +63,8 @@ def test_pi_stacking_individual():
 def test_find_pi_stacking_rings():
     """Tests that when a list of rings is passed through the function two lists
     are created that have a known length for stacking and not stacking"""
-    ring_list = armadillo.find_all_rings(u)
-    thio_stacking = armadillo.find_pi_stacking_rings(ring_list, u, distmax=5.0, distmin=2.0,
+
+    ring_list = armadillo.find_all_rings(ag, u)
+    thio_stacking, t_shaped = armadillo.find_pi_stacking_rings(ring_list, ag, distmax=5.0, distmin=2.0,
                                angle_threshold_parallel=45, angle_threshold_tshaped=80)
-    assert len(thio_stacking) == 135
+    assert len(thio_stacking) == 118 and len(t_shaped) == 17
